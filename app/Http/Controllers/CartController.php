cd82c9d33c33d300e2780c966d15d464a5fe6df1
SinghJyotiRajesh <jyotirajeshsingh173@gmail.com>
2025-04-08 14:19:31 +0530

    push this code

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Overtrue\LaravelShoppingCart\Cart;
+
+
+class CartController extends Controller
+{
+    public function index()
+    {
+        
+        $items = Cart::instance('cart')->content();
+        return view('cart', compact('items'));
+    }
+}

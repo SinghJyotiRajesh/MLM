e388a10490bb3fa91ab92fad5043a67370b713c4
SinghJyotiRajesh <jyotirajeshsingh173@gmail.com>
2025-04-25 16:38:54 +0530

    Homecontroller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Slide;
+use App\Models\Category;
+use App\Models\Product;
 
 class HomeController extends Controller
 {
@@ -11,6 +13,9 @@ class HomeController extends Controller
     public function index()
     {
         $slides = Slide::where('status',1)->get()->take(3);
-        return view('index', compact('slides'));
+        $categories = Category::orderBy('name')->get();
+        $sproducts = Product::whereNotNull('sale_price')->where('sale_price','<>','')->inRandomOrder()->get()->take(8);
+        $fproducts = Product::where('featured',1)->get()->take(8);
+        return view('index', compact('slides','categories','sproducts','fproducts'));
     }
 }
